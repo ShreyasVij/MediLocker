2cb76b03f32fb98714b4434bd11b5734ff19cfb3
ShreyasVij <shrvij28@gmail.com>
2025-11-05 17:24:02 +0530

    Made the Navbar

---
+++ src/landing page/Home/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='container p-5 mb-5'>
+      <div className='row text-center'>
+        <img src='media/images/homeHero.png' alt='Hero Image'className='mb-5' />
+        <h1 className='mt-5'>MediLocker</h1>
+        <p>Online plateform to store all of our files</p>
+    </div>
+    </div>
+  )
+}
+
+export default Hero
